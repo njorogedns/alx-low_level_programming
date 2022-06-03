236bd4770ed21648dc5cddd315508f2b992cc46b
njorogedns <njorogedns13@gmail.com>
2022-06-03 04:12:41 -0700

    prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(void)
+{
+int i = '0';
+int j = '0';
+while (i <= '9')
+{
+while (j <= '9')
+{
+putchar(i);
+putchar(j);
+if (j == '9' && i == '9')
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+j++;
+}
+if (j >= '9')
+{
+j = '0';
+}
+i++;
+}
+return (0);
+}
