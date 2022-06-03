f3b41433bb26797ccf8064f0dabae2219b9e7a00
njorogedns <njorogedns13@gmail.com>
2022-06-03 04:23:59 -0700

    prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,30 +6,31 @@
 */
 int main(void)
 {
-int i = '0';
-int j = '0';
-while (i <= '9')
-{
-while (j <= '9')
-{
-putchar(i);
-putchar(j);
-if (j == '9' && i == '9')
-{
-putchar('\n');
-}
-else
-{
-putchar(',');
-putchar(' ');
-}
-j++;
-}
-if (j >= '9')
-{
-j = '0';
-}
-i++;
-}
-return (0);
+  int i = '0';
+  int j = '0';
+
+  while (i <= '9')
+    {
+      while (j <= '9')
+	{
+	  if (!(i > j || i == j))
+	    {
+	      putchar(i);
+	      putchar(j);
+	      if (i == '8' && j == '9')
+		{
+		  putchar('\n');
+		}
+	      else
+		{
+		  putchar(',');
+		  putchar(' ');
+		}
+	    }
+	  j++;
+	}
+      j = '0';
+      i++;
+    }
+  return (0);
 }
