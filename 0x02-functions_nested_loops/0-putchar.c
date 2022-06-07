e10989908bf874424d99c32a09ae94a939cb8f80
njorogedns <njorogedns13@gmail.com>
2022-06-07 08:29:04 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-int _putchar(char c)
+/**
+ * main - first point
+ * Return: always 0
+ */
+int main(void)
 {
-  return (write(1, &c, 1));
-}
-
-int main ()
-{
-
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
-
-
-  return 0;
-
-
+  char num[] = "_putchar";
+  int i;
+  for (i = 0; i < 8; i++)
+    {
+      _putchar(num[i]);
+    }
+  putchar('\n');
+  return (0);
 }
