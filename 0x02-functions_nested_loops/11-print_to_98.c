29138b4c8dd8da0ea8c87ea52796c1105820310f
njorogedns <njorogedns13@gmail.com>
2022-06-08 13:33:24 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,30 +7,29 @@
  */
 void print_to_98(int n)
 {
-  int i;
+int i;
 
-  if (n > 98)
-    {
-      for (i = n; i > 97; i--)
-	{
-	  printf("%d", i);
-	  if (i != 98)
-	    {
-	      printf(", ");
-	    }
-	}
-    }
-  else
-    {
-      for (i = n; i < 99; i++)
-	{
-	  printf("%d", i);
-	  if (i != 98)
-	    {
-	      printf(", ");
-	    }
-	}
-
-    }
-  printf("\n");
+if (n > 98)
+{
+for (i = n; i > 97; i--)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+}
+else
+{
+for (i = n; i < 99; i++)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+}
+printf("\n");
 }
